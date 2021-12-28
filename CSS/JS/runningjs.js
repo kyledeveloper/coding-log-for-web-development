ba354d2d92dfdbdfbaa81792cb58bafbc91398ae
kyledeveloper <kerwinliang6@gmail.com>
2021-12-27 19:48:18 -0800

    Update runningjs.js

--- CSS/JS/runningjs.js
+++ CSS/JS/runningjs.js
@@ -1,1 +1,17 @@
-console.log("HI there")
+console.log("HI there");
+// const age = prompt("Input your age");
+// if (age<10){
+//     console.log("too young");
+// }
+// else if ( 10 <= age <=20){
+//     console.log("Target age");
+// }
+// else {
+//     console.log("too old");
+// }
+
+let numArray = [1,2,3,4,5,6]
+numArray[numArray.length] = 7
+console.log(numArray)
+numArray.push(8)
+console.log(numArray)
